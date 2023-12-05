8ad5be8879521cdc51e6cb4b617a58437dda2d50
Cameron Shearer <cameronleeshearer@gmail.com>
2023-12-05 02:20:09 +0000

    started insertions

--- inserts.sql
+++ inserts.sql
@@ -1,1 +1,164 @@
+-- THE MAJORITY OF THIS FILE WAS GENERATED USING 
+-- https://www.morckaroo.com 
+
 -- INSERTS
+
+insert into country (country_id, country_name) values (1, 'Czech Republic');
+insert into country (country_id, country_name) values (2, 'Sweden');
+insert into country (country_id, country_name) values (3, 'Japan');
+insert into country (country_id, country_name) values (4, 'Cameroon');
+insert into country (country_id, country_name) values (5, 'Brazil');
+insert into country (country_id, country_name) values (6, 'Russia');
+insert into country (country_id, country_name) values (7, 'Uganda');
+insert into country (country_id, country_name) values (8, 'Poland');
+insert into country (country_id, country_name) values (9, 'Ecuador');
+insert into country (country_id, country_name) values (10, 'China');
+insert into country (country_id, country_name) values (11, 'Philippines');
+insert into country (country_id, country_name) values (12, 'United States');
+insert into country (country_id, country_name) values (13, 'Uzbekistan');
+insert into country (country_id, country_name) values (14, 'United Kingdom of Great Britain and Northern Ireland');
+insert into country (country_id, country_name) values (15, 'Turkmenistan');
+insert into country (country_id, country_name) values (16, 'Yemen');
+insert into country (country_id, country_name) values (17, 'China');
+insert into country (country_id, country_name) values (18, 'Kazakhstan');
+insert into country (country_id, country_name) values (19, 'Ireland');
+insert into country (country_id, country_name) values (20, 'Ghana');
+insert into country (country_id, country_name) values (21, 'Morocco');
+insert into country (country_id, country_name) values (22, 'Egypt');
+insert into country (country_id, country_name) values (23, 'Malta');
+insert into country (country_id, country_name) values (24, 'Portugal');
+insert into country (country_id, country_name) values (25, 'Indonesia');
+
+insert into city (city_id, country_id, city_name) values (1, 9, 'Utah');
+insert into city (city_id, country_id, city_name) values (2, 8, 'Randy');
+insert into city (city_id, country_id, city_name) values (3, 25, 'Valley Edge');
+insert into city (city_id, country_id, city_name) values (4, 5, 'Melvin');
+insert into city (city_id, country_id, city_name) values (5, 24, 'Luster');
+insert into city (city_id, country_id, city_name) values (6, 6, 'Grover');
+insert into city (city_id, country_id, city_name) values (7, 3, 'Ruskin');
+insert into city (city_id, country_id, city_name) values (8, 12, 'Crest Line');
+insert into city (city_id, country_id, city_name) values (9, 14, 'Center');
+insert into city (city_id, country_id, city_name) values (10, 14, 'Vermont');
+insert into city (city_id, country_id, city_name) values (11, 8, 'Golf Course');
+insert into city (city_id, country_id, city_name) values (12, 1, 'Havey');
+insert into city (city_id, country_id, city_name) values (13, 3, 'Cherokee');
+insert into city (city_id, country_id, city_name) values (14, 3, 'Truax');
+insert into city (city_id, country_id, city_name) values (15, 10, 'Carpenter');
+insert into city (city_id, country_id, city_name) values (16, 23, 'Daystar');
+insert into city (city_id, country_id, city_name) values (17, 10, 'Thackeray');
+insert into city (city_id, country_id, city_name) values (18, 11, 'Macpherson');
+insert into city (city_id, country_id, city_name) values (19, 17, 'Old Shore');
+insert into city (city_id, country_id, city_name) values (20, 23, 'Summer Ridge');
+insert into city (city_id, country_id, city_name) values (21, 11, 'Hollow Ridge');
+insert into city (city_id, country_id, city_name) values (22, 23, 'Bluestem');
+insert into city (city_id, country_id, city_name) values (23, 12, 'Utah');
+insert into city (city_id, country_id, city_name) values (24, 16, '5th');
+insert into city (city_id, country_id, city_name) values (25, 1, 'Utah');
+insert into city (city_id, country_id, city_name) values (26, 8, 'Fairfield');
+insert into city (city_id, country_id, city_name) values (27, 20, 'Mendota');
+insert into city (city_id, country_id, city_name) values (28, 22, 'Portage');
+insert into city (city_id, country_id, city_name) values (29, 4, 'Heath');
+insert into city (city_id, country_id, city_name) values (30, 24, 'Anderson');
+insert into city (city_id, country_id, city_name) values (31, 12, 'Manley');
+insert into city (city_id, country_id, city_name) values (32, 9, 'Chinook');
+insert into city (city_id, country_id, city_name) values (33, 18, 'Barby');
+insert into city (city_id, country_id, city_name) values (34, 15, 'Carioca');
+insert into city (city_id, country_id, city_name) values (35, 15, 'Russell');
+insert into city (city_id, country_id, city_name) values (36, 1, 'Mandrake');
+insert into city (city_id, country_id, city_name) values (37, 19, 'Reindahl');
+insert into city (city_id, country_id, city_name) values (38, 19, 'Anniversary');
+insert into city (city_id, country_id, city_name) values (39, 23, 'Oxford');
+insert into city (city_id, country_id, city_name) values (40, 10, 'Gulseth');
+insert into city (city_id, country_id, city_name) values (41, 24, 'Goodland');
+insert into city (city_id, country_id, city_name) values (42, 18, 'Dottie');
+insert into city (city_id, country_id, city_name) values (43, 4, 'School');
+insert into city (city_id, country_id, city_name) values (44, 17, 'Stoughton');
+insert into city (city_id, country_id, city_name) values (45, 9, 'Tony');
+insert into city (city_id, country_id, city_name) values (46, 9, 'Rutledge');
+insert into city (city_id, country_id, city_name) values (47, 2, 'Eagan');
+insert into city (city_id, country_id, city_name) values (48, 9, 'Buhler');
+insert into city (city_id, country_id, city_name) values (49, 11, 'Lake View');
+insert into city (city_id, country_id, city_name) values (50, 14, 'Portage');
+
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (1, 5, 'XN6 2SJL', 'Nelson', 'Birchwood');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (2, 21, 'TB5 5DTW', 'Kenwood', 'Stuart');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (3, 50, 'HW6 7CHJ', 'Coleman', 'Kinsman');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (4, 38, 'JA8 7YUK', 'Waubesa', 'Caliangt');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (5, 11, 'OK1 1RLT', 'Dexter', 'Pearson');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (6, 44, 'KO1 5OAB', 'Northview', 'Burning Wood');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (7, 12, 'AK5 5NJF', 'Northridge', 'Alpine');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (8, 4, 'AR5 2SQI', 'Hintze', 'Mallory');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (9, 10, 'PM0 3TDV', 'Graceland', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (10, 21, 'PC3 2RCH', 'Jenna', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (11, 2, 'MF3 5BIW', 'Kennedy', 'Corry');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (12, 20, 'YP6 5OLN', 'Columbus', 'Sunbrook');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (13, 46, 'HT1 9SQZ', 'Comanche', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (14, 45, 'CY1 0XMS', 'Kingsford', 'Novick');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (15, 20, 'KW8 3UCI', 'Harper', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (16, 47, 'SN0 8JLL', 'Merrick', '6th');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (17, 29, 'PF8 7GYR', 'Muir', 'Wayridge');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (18, 7, 'SX9 5KTX', 'Mallory', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (19, 35, 'OA0 4OJL', 'Pankratz', 'Crest Line');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (20, 37, 'DS9 0GEG', 'Waxwing', 'Waywood');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (21, 1, 'US2 6GMX', 'Northwestern', 'Hollow Ridge');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (22, 7, 'PM4 6SXB', 'Westport', 'Evergreen');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (23, 21, 'UE2 1IDI', 'Mitchell', 'Myrtle');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (24, 41, 'XO1 2BTN', 'Sunnyside', 'Gulseth');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (25, 18, 'NO3 8ZSG', 'Mallard', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (26, 26, 'RC1 5YRC', 'Kedzie', 'Waywood');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (27, 17, 'CE2 0ZYH', 'Heffernan', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (28, 15, 'LJ0 2HNG', 'Hazelcrest', 'Spohn');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (29, 48, 'RD9 3DCY', 'Springs', 'Lyons');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (30, 29, 'AC2 0UKO', '2nd', 'Arkansas');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (31, 9, 'VV2 7HNV', 'School', 'Vera');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (32, 48, 'BD5 9APQ', 'North', 'Granby');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (33, 16, 'VG2 9SLG', 'Milwaukee', 'Towne');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (34, 3, 'CN5 7QGD', 'Calypso', 'Roth');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (35, 40, 'NT2 0GTE', 'Kropf', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (36, 2, 'BZ5 8TEE', 'Helena', 'Crescent Oaks');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (37, 25, 'WF5 2GOX', 'Manufacturers', 'Grim');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (38, 4, 'VP1 3TYG', 'Garrison', 'Namekagon');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (39, 46, 'OM7 3PGC', 'Redwing', 'Messerschmidt');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (40, 13, 'YG7 0YAC', 'Eliot', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (41, 11, 'TO1 2RHO', 'Ruskin', 'Forster');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (42, 29, 'FK5 5RPK', 'Prentice', 'Lakewood');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (43, 13, 'WP1 4BEO', '2nd', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (44, 4, 'FM7 1ZPE', 'Bellgrove', '7th');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (45, 46, 'CU6 8MND', 'Di Loreto', 'Grim');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (46, 19, 'KD5 7PAI', 'Monica', 'Clemons');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (47, 30, 'UN4 3WGF', 'Anthes', 'Shopko');
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (48, 17, 'QB4 2CME', 'Anthes', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (49, 11, 'UI6 8QLN', 'Mosinee', null);
+insert into address (address_id, city_id, address_postcode, address_one, address_two) values (50, 19, 'VG1 6YNH', 'Rutledge', 'Judy');
+
+-- GENERATED OURSELVES
+
+INSERT INTO facilities (facilities_id, facilities_name)
+VALUES (1, ''),
+VALUES (2, ''),
+VALUES (3, ''),
+VALUES (4, ''),
+VALUES (5, ''),
+VALUES (6, ''),
+VALUES (7, ''),
+VALUES (8, ''),
+VALUES (9, ''),
+VALUES (10, '');
+
+INSERT INTO yard_facilities(yard_id, facilities_id)
+VALUES ( , ),
+VALUES ( , ),
+VALUES ( , ),
+VALUES ( , ),
+VALUES ( , ),
+VALUES ( , ),
+VALUES ( , ),
+VALUES ( , ),
+VALUES ( , ),
+VALUES ( , );
+
+
+
+
+
+
