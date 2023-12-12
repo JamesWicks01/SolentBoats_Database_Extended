c3b4bd7b51f4236dd04b9cb202f9d90ccde3c2ff
JamesWicks01 <115103781+JamesWicks01@users.noreply.github.com>
2023-12-12 11:03:15 +0000

    Finished Roles

--- roles.sql
+++ roles.sql
@@ -26,18 +26,42 @@ GRANT SELECT ON staff_work_yard TO general;
 CREATE ROLE technician LOGIN;
 -- Grants the role technician with permissions to connect to the database
 GRANT CONNECT ON DATABASE grp21_solent TO technician;
+-- Grants the role with the relevant permissions to access the services section
+GRANT SELECT, INSERT, UPDATE ON manufacture TO technician;
+GRANT SELECT, INSERT, UPDATE ON boat TO technician;
+GRANT SELECT, INSERT, UPDATE ON "service" TO technician;
+GRANT SELECT, INSERT, UPDATE ON staff_service TO technician;
+GRANT SELECT, INSERT, UPDATE ON fault TO technician;
+GRANT SELECT, INSERT, UPDATE ON service_fault TO technician;
+GRANT SELECT, INSERT, UPDATE ON history TO technician;
 
 -- Engine Technician 
 -- Creating a role called engine technician with login permissions
 CREATE ROLE engine_technician LOGIN;
 -- Grants the role engine technician with permissions to connect to the database
 GRANT CONNECT ON DATABASE grp21_solent TO engine_technician;
+-- Grants the role with the relevant permissions to access the services section
+GRANT SELECT, INSERT, UPDATE ON manufacture TO engine_technician;
+GRANT SELECT, INSERT, UPDATE ON boat TO engine_technician;
+GRANT SELECT, INSERT, UPDATE ON "service" TO engine_technician;
+GRANT SELECT, INSERT, UPDATE ON staff_service TO engine_technician;
+GRANT SELECT, INSERT, UPDATE ON fault TO engine_technician;
+GRANT SELECT, INSERT, UPDATE ON service_fault TO engine_technician;
+GRANT SELECT, INSERT, UPDATE ON history TO engine_technician;
 
 -- Hull Specialist
 -- Creating a role called hull specialist with login permissions
 CREATE ROLE hull_specialist LOGIN;
 -- Grants the role hull specialist with permissions to connect to the database
 GRANT CONNECT ON DATABASE grp21_solent TO hull_specialist;
+-- Grants the role with the relevant permissions to access the services section
+GRANT SELECT, INSERT, UPDATE ON manufacture TO hull_specialist;
+GRANT SELECT, INSERT, UPDATE ON boat TO hull_specialist;
+GRANT SELECT, INSERT, UPDATE ON "service" TO hull_specialist;
+GRANT SELECT, INSERT, UPDATE ON staff_service TO hull_specialist;
+GRANT SELECT, INSERT, UPDATE ON fault TO hull_specialist;
+GRANT SELECT, INSERT, UPDATE ON service_fault TO hull_specialist;
+GRANT SELECT, INSERT, UPDATE ON history TO hull_specialist;
 
 
 
